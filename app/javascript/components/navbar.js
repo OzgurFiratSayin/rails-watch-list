06b0f690f0608e547aebe0c8dc9ab5cb0936cc6c
Ozgur Firat Sayin <ozgurfiratsayin@gmail.com>
2021-05-30 15:21:21 +0100

    Stlying

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -1,8 +1,11 @@
 const initUpdateNavbarOnScroll = () => {
     const navbar = document.querySelector('.navbar');
     if (navbar) {
+      let banner = undefined;
+      document.querySelector('.banner-main') ? banner = document.querySelector('.banner-main') : banner = document.querySelector('.banner-list')
+      let bannerHeight = banner.offsetHeight;
       window.addEventListener('scroll', () => {
-        if (window.scrollY >= window.innerHeight) {
+        if (window.scrollY >= bannerHeight) {
           navbar.classList.add('navbar-grey');
         } else {
           navbar.classList.remove('navbar-grey');
