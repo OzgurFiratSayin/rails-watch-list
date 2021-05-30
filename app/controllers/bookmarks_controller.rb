cfd43f6503be180681c8c59dfb3b359002e109e7
Ozgur Firat Sayin <ozgurfiratsayin@gmail.com>
2021-05-30 13:34:50 +0100

    Fixes and styling, and js for navbar

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -16,8 +16,9 @@ class BookmarksController < ApplicationController
 
   def destroy
     @bookmark = Bookmark.find(params[:id])
+    @list = List.find(@bookmark.list_id)
     @bookmark.destroy
-    redirect_to lists_path
+    redirect_to list_path(@list)
   end
 
   private
